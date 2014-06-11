13b75367274cf4dd9754a945f23eab3d57bd22d0
Serafeim Maroulis <seramak@gmail.com>
2014-06-11 17:08:24 +0100

    registration form started and user model created

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 gem 'geocoder'
-
+gem 'devise'
 
 group :development, :test do
   gem 'rspec-rails', '~> 3.0.0.beta'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
