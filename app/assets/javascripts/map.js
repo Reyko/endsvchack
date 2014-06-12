b8864b3166945065f55c29edc23ce6113ff51faf
Serafeim Maroulis <seramak@gmail.com>
2014-06-12 07:08:08 +0100

    more styles

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -5,12 +5,16 @@ $( document ).ready(function() {
 
 function initialize() {
   var mapOptions = {
-    center: new google.maps.LatLng(71.1333, 27.7000),
-    zoom: 3,
+    center: new google.maps.LatLng(51.5072, 0.1275),
+    zoom: 5,
     scrollwheel: false
   };
   var map = new google.maps.Map(document.getElementById("map-canvas"),
       mapOptions);
 }
 google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  geocoded_by :location   # can also be an IP address
+  after_validation :geocode          # auto-fetch coordinates
+
+
+
   validates :first_name, :presence => true
   validates :last_name, :presence => true
 
@@ -8,4 +13,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   belongs_to :country
+
+  def location
+    location = "#{country.name},#{:city}"
+    location
+  end
 end
